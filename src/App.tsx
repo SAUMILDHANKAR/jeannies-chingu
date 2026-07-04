6644c53b573b0c9989aaa16d456db7aff6f9b70c
Saumil Dhankar <saumil.dhankar@yahoo.com>
2026-07-04 23:44:43 +0000

    sign in component ui visible

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import SignIn from './components/SignIn'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <SignIn />
         <p>
           Upcoming form component.
         </p>
