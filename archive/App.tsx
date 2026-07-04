e793367f98caffcb4edc45f157b78a80101edaf6
Saumil Dhankar <saumil.dhankar@yahoo.com>
2026-07-04 22:34:23 +0000

    initial app.tsx cleanup

--- archive/App.tsx
+++ archive/App.tsx
@@ -9,21 +9,24 @@ function App() {
   return (
     <>
       <div>
-        <p>
-          Welcome to the application
-        </p>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <h1>Jeannie's chingu</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Upcoming form component.
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Thanks for visiting the site
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
