8471d3ac04ddd9f2c11fe6687ef7f1f86943c8af
Saumil Dhankar <saumil.dhankar@yahoo.com>
2026-07-17 17:36:44 -0700

    copilot teaches me routing :(

--- src/App.tsx
+++ src/App.tsx
@@ -16,19 +16,23 @@ function App() {
     <>
       
       <h1>Jeannie's chingu</h1>
-      
-        
-        <SignIn />
+	  
       
 	  <Box>
 	    <Suspense>
 		<Routes>
+		{/* Default route shows SignIn */}
+		{/* For evaluators: unfortunately even after spending about half an hour researching documentation I was not able to figure out the default route functionlity to get signin component to not show once we are on dashboard path, so used copilot help */}
+            <Route path="/" element={<SignIn />} />
+
+           
 	  {/* Dashboard */}
 	      <Route path="/Dashboard" element={<Dashboard />} />
 
 	    </Routes>
 	  </Suspense>
-	</Box>	
+	</Box>	      
+
     </>
   )
 }
